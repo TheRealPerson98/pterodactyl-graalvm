bc8ad342ac7bdb469635af45d1622b747c71e010
TheRealPerson98 <jacesleeman603@gmail.com>
2025-03-23 11:19:24 -0400

    Fix GraalJS and Node.js installation in Java 21 image

--- java21/entrypoint.sh
+++ java21/entrypoint.sh
@@ -4,6 +4,15 @@ cd /home/container
 # Output Current Java Version
 java -version
 
+# Test JavaScript and Node.js
+echo "Testing JavaScript..."
+echo "console.log('JavaScript test from GraalVM');" > test.js
+js test.js
+echo "Testing Node.js..."
+echo "console.log('Node.js test from GraalVM');" > test-node.js
+node test-node.js
+rm -f test.js test-node.js
+
 # Replace Startup Command
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
