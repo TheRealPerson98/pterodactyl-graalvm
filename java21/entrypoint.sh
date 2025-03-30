e528529e127d2a1f9886f2a49205720ee8c98f37
TheRealPerson98 <jacesleeman603@gmail.com>
2025-03-30 12:19:10 -0400

    ✨ feat(build): integrate graalvm polyglot api support
    - adds polyglot api test during entrypoint execution
    - configures classpath to include graalvm javascript libraries
    - modifies startup command to include graalvm libraries for java execution

--- java21/entrypoint.sh
+++ java21/entrypoint.sh
@@ -4,16 +4,9 @@ cd /home/container
 # Output Current Java Version
 java -version
 
-# Test JavaScript and Node.js
-echo "Testing JavaScript..."
-echo "console.log('JavaScript test from GraalVM');" > test.js
-js test.js
-echo "Testing Node.js..."
-echo "console.log('Node.js test from GraalVM');" > test-node.js
-node test-node.js
-
 # Test Polyglot API
 echo "Testing Polyglot API..."
+# Test Polyglot API
 cat > test-polyglot.java << EOF
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
@@ -39,14 +32,35 @@ public class TestPolyglot {
 EOF
 
 javac -cp "$JAVA_HOME/lib/graal-sdk-23.0.1.jar" test-polyglot.java
+if [ $? -ne 0 ]; then
+    echo "WARNING: Failed to compile Polyglot test!"
+    ls -la $JAVA_HOME/lib/graal-sdk*
+fi
+
 java -cp ".:$JAVA_HOME/lib/graal-sdk-23.0.1.jar" TestPolyglot
+if [ $? -ne 0 ]; then
+    echo "WARNING: Failed to run Polyglot test!"
+fi
 
 # Clean up test files
 rm -f test.js test-node.js test-polyglot.java TestPolyglot.class
+
+# Set up classpath for the server to access GraalVM JavaScript
+export CLASSPATH="$CLASSPATH:$JAVA_HOME/lib/graal-sdk-23.0.1.jar:$JAVA_HOME/lib/js-23.0.1.jar:$JAVA_HOME/lib/truffle-api-23.0.1.jar"
+
+# Add the GraalVM libraries to the Java class path when starting the server
+GRAALVM_LIBS="-Djava.class.path=$JAVA_HOME/lib/graal-sdk-23.0.1.jar:$JAVA_HOME/lib/js-23.0.1.jar:$JAVA_HOME/lib/truffle-api-23.0.1.jar"
 
 # Replace Startup Command
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Prepare any GRAALVM_LIBS if needed for the java command
+if [[ $MODIFIED_STARTUP == *"java "* ]]; then
+    # Only modify java commands
+    MODIFIED_STARTUP=${MODIFIED_STARTUP/java /java $GRAALVM_LIBS /}
+fi
+
 # Run the Server
+echo "Starting server with: $MODIFIED_STARTUP"
 eval ${MODIFIED_STARTUP} 
